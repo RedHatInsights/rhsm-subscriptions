b7d03f6e92a47cfaab1d6b8c5417b826dd64cda2
Kevin Howell <khowell@redhat.com>
2019-04-23 15:25:18 -0400

    Implement the debug endpoint
    With this change the debug endpoint accurately reflects what would be
    sent to the inventory service.
    
    Since we control the interface for it, I decided that ConsumerInventory
    should reflect our service's view of a consumer generally, and then
    realized that there was a great deal of overlap with ConduitFacts.
    
    Since the validation provided by ConduitFacts has been valuable, I
    reworked it to be a subclass of ConsumerInventory.
    
    With this in place, `curl localhost:8080/rhsm-conduit/v1/inventories/1234`
    (or similar) is useful.

--- src/main/java/org/candlepin/insights/inventory/ConduitFacts.java
+++ src/main/java/org/candlepin/insights/inventory/ConduitFacts.java
@@ -20,149 +20,57 @@
  */
 package org.candlepin.insights.inventory;
 
+import org.candlepin.insights.api.model.ConsumerInventory;
 import org.candlepin.insights.validator.ip.IpAddress;
 
 import org.hibernate.validator.constraints.Length;
 
 import java.util.List;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
 /**
- * POJO that holds all facts scoped for collection by the conduit.
+ * POJO that validates all facts scoped for collection by the conduit.
  */
-public class ConduitFacts {
-
-    private String subscriptionManagerId;
-    private String biosUuid;
-    private String orgId;
-
-    private List<@IpAddress String> ipAddresses;
+public class ConduitFacts extends ConsumerInventory {
 
     @Length(min = 1, max = 255)
+    @Override
+    public String getFqdn() {
+        return super.getFqdn();
+    }
 
-    private String fqdn;
+    @Valid
+    @Override
+    public List<@IpAddress String> getIpAddresses() {
+        return super.getIpAddresses();
+    }
 
     // See https://stackoverflow.com/a/4260512/6124862
     // Also a soft validation.  A mixed delimiter MAC like a1:b2-c3:d4-e5:f6 will still validate
-    private List<@Pattern(regexp = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$") String> macAddresses;
+    @Valid
+    @Override
+    public List<@Pattern(regexp = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$") String> getMacAddresses() {
+        return super.getMacAddresses();
+    }
 
     @Positive
-    private Integer cpuSockets;
-
-    @Positive
-    private Integer cpuCores;
-
-    @Positive
-    private Integer memory;
-
-    private String architecture;
-    private Boolean isVirtual;
-    private String vmHost;
-    private List<String> rhProd;
-
-    public String getSubscriptionManagerId() {
-        return subscriptionManagerId;
-    }
-
-    public void setSubscriptionManagerId(String subscriptionManagerId) {
-        this.subscriptionManagerId = subscriptionManagerId;
-    }
-
-    public String getBiosUuid() {
-        return biosUuid;
-    }
-
-    public String getOrgId() {
-        return orgId;
-    }
-
-    public void setOrgId(String orgId) {
-        this.orgId = orgId;
-    }
-
-    public void setBiosUuid(String biosUuid) {
-        this.biosUuid = biosUuid;
-    }
-
-    public List<String> getIpAddresses() {
-        return ipAddresses;
-    }
-
-    public void setIpAddresses(List<String> ipAddresses) {
-        this.ipAddresses = ipAddresses;
-    }
-
-    public String getFqdn() {
-        return fqdn;
-    }
-
-    public void setFqdn(String fqdn) {
-        this.fqdn = fqdn;
-    }
-
-    public List<String> getMacAddresses() {
-        return macAddresses;
-    }
-
-    public void setMacAddresses(List<String> macAddresses) {
-        this.macAddresses = macAddresses;
-    }
-
+    @Override
     public Integer getCpuSockets() {
-        return cpuSockets;
+        return super.getCpuSockets();
     }
 
-    public void setCpuSockets(Integer cpuSockets) {
-        this.cpuSockets = cpuSockets;
-    }
-
+    @Positive
+    @Override
     public Integer getCpuCores() {
-        return cpuCores;
+        return super.getCpuCores();
     }
 
-    public void setCpuCores(Integer cpuCores) {
-        this.cpuCores = cpuCores;
-    }
-
+    @Positive
+    @Override
     public Integer getMemory() {
-        return memory;
-    }
-
-    public void setMemory(Integer memory) {
-        this.memory = memory;
-    }
-
-    public String getArchitecture() {
-        return architecture;
-    }
-
-    public void setArchitecture(String architecture) {
-        this.architecture = architecture;
-    }
-
-    public String getVmHost() {
-        return vmHost;
-    }
-
-    public Boolean getVirtual() {
-        return isVirtual;
-    }
-
-    public void setVirtual(Boolean virtual) {
-        isVirtual = virtual;
-    }
-
-    public void setVmHost(String vmHost) {
-        this.vmHost = vmHost;
-    }
-
-    public List<String> getRhProd() {
-        return rhProd;
-    }
-
-    public void setRhProd(List<String> rhProd) {
-        this.rhProd = rhProd;
+        return super.getMemory();
     }
 }
