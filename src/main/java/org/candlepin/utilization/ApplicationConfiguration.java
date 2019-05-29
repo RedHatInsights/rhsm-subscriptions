53519d83512fda8359ca92ef6af6e9fc07329e93
Michael Stead <mstead@redhat.com>
2019-05-29 12:54:46 -0300

    Adding some core components to the project
    * Support releases and gradle git extensions
    * Set up servlet path
    * Added application configuration properties stub
    * Small src code path restructure to make the main app code
      part of the root project.

---
+++ src/main/java/org/candlepin/utilization/ApplicationConfiguration.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2009 - 2019 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.utilization;
+
+import org.jboss.resteasy.springboot.ResteasyAutoConfiguration;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.validation.Validator;
+
+
+/** Class to hold configuration beans */
+@Configuration
+@Import(ResteasyAutoConfiguration.class) // needed to be able to reference ResteasyApplicationBuilder
+@EnableConfigurationProperties(ApplicationProperties.class)
+// The values in application.yaml should already be loaded by default
+@PropertySource("classpath:/subscriptions.properties")
+public class ApplicationConfiguration implements WebMvcConfigurer {
+
+    /**
+     * Tell Spring AOP to run methods in classes marked @Validated through the JSR-303 Validation
+     * implementation.  Validations that fail will throw an ConstraintViolationException.
+     * @return post-processor used by Spring AOP
+     */
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor servletInitializer() {
+        return new JaxrsApplicationServletInitializer();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //TODO Enable once openapi spec is implemented
+//        registry.addViewController("/api-docs").setViewName("redirect:/api-docs/index.html");
+//        registry.addViewController("/api-docs/").setViewName("redirect:/api-docs/index.html");
+    }
+}
