478184cabea7adb7b30a054adcff9593567cb79b
Alex Wood <awood@redhat.com>
2019-06-14 16:43:56 -0400

    Rename the project to rhsm-subscriptions

---
+++ src/main/java/org/candlepin/subscriptions/jackson/ObjectMapperContextResolver.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2009 - 2019 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.jackson;
+
+import org.candlepin.subscriptions.ApplicationProperties;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+
+/**
+ * A ContextResolver responsible for customizing the configuration of Jackson's
+ * ObjectMapper instance. This is marked as a Provider so that RestEasy will
+ * use the configured ObjectMapper provided by this instance.
+ */
+@Provider
+public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper objectMapper;
+
+    public ObjectMapperContextResolver(ApplicationProperties applicationProperties) {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, applicationProperties.isPrettyPrintJson());
+        objectMapper.setSerializationInclusion(Include.NON_NULL);
+        objectMapper.setSerializationInclusion(Include.NON_EMPTY);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        // Tell the mapper to check the classpath for any serialization/deserialization modules
+        // such as the Java8 date/time module (JavaTimeModule).
+        objectMapper.findAndRegisterModules();
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> objectType) {
+        return objectMapper;
+    }
+
+}
