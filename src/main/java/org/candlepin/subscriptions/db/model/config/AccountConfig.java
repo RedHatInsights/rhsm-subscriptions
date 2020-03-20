d0db09b556e548531d35a84326ef976a9c25fb34
Michael Stead <mstead@redhat.com>
2020-03-20 14:17:50 -0300

    Add DB schema and repositories for opt-in functionality
    AccountConfig and OrgConfig entities will be used to represent
    the account and org configurations for tally/conduit that will
    enable/disable various features such as syncing data and reporting.

---
+++ src/main/java/org/candlepin/subscriptions/db/model/config/AccountConfig.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2009 - 2019 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.db.model.config;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * Represents the configuration properties for an account.
+ */
+@Entity
+@Table(name = "account_config")
+public class AccountConfig extends BaseConfig {
+
+    @Id
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "reporting_enabled")
+    private Boolean reportingEnabled;
+
+    public AccountConfig() {
+    }
+
+    public AccountConfig(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Boolean getReportingEnabled() {
+        return reportingEnabled;
+    }
+
+    public void setReportingEnabled(Boolean reportingEnabled) {
+        this.reportingEnabled = reportingEnabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof AccountConfig)) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        AccountConfig that = (AccountConfig) o;
+        return Objects.equals(accountNumber, that.accountNumber) &&
+                   Objects.equals(reportingEnabled, that.reportingEnabled);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), accountNumber, reportingEnabled);
+    }
+
+}
---
+++ src/main/java/org/candlepin/subscriptions/db/model/config/OrgConfig.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2009 - 2019 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.db.model.config;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Represents the configuration properties for an organization (owner).
+ */
+@Entity
+@Table(name = "org_config")
+public class OrgConfig extends BaseConfig {
+
+    @Id
+    @Column(name = "org_id")
+    private String orgId;
+
+    public OrgConfig() {
+    }
+
+    public OrgConfig(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof OrgConfig)) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        OrgConfig orgConfig = (OrgConfig) o;
+        return Objects.equals(orgId, orgConfig.orgId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), orgId);
+    }
+}
