2aaefe42eedd10836599de2d20bd1a073fc4eb46
Kevin Howell <khowell@redhat.com>
2020-10-06 13:13:46 -0400

    pr_check.sh: Cleanup containers after tests

--- pr_check.sh
+++ pr_check.sh
@@ -6,6 +6,6 @@ IMAGE="quay.io/cloudservices/rhsm-subscriptions"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
 docker build -f Dockerfile.test -t "${IMAGE}:${IMAGE_TAG}" .
-docker run "${IMAGE}:${IMAGE_TAG}" /tmp/src/gradlew --no-daemon test
-docker run "${IMAGE}:${IMAGE_TAG}" /tmp/src/gradlew --no-daemon checkstyleMain checkstyleTest
+docker run --rm "${IMAGE}:${IMAGE_TAG}" /tmp/src/gradlew --no-daemon test
+docker run --rm "${IMAGE}:${IMAGE_TAG}" /tmp/src/gradlew --no-daemon checkstyleMain checkstyleTest
 docker image rm "${IMAGE}:${IMAGE_TAG}"
