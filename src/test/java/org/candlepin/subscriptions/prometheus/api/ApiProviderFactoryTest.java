74d6979d1c6f49365bb41ff5f8351ce490f4f5b2
Michael Stead <mstead@redhat.com>
2020-11-18 09:04:50 -0400

    Add prometheus generated client for query and range_query API
    Sadly, instant and matrix prometheus values stored in same object.
    Due to current limitations of openapi java code gen, I couldn't
    get the API to allow different response types. Instead of spinning
    wheels here, I decided to just include both a 'value' and 'values'
    field on the result that will get filled out appropriately in the
    response.
    
    Matrix vector results will fill out 'values'. Instance vector
    results will fill out the 'value' field.

---
+++ src/test/java/org/candlepin/subscriptions/prometheus/api/ApiProviderFactoryTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019 - 2020 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.prometheus.api;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.candlepin.subscriptions.http.HttpClientProperties;
+import org.candlepin.subscriptions.prometheus.api.ApiProvider;
+import org.candlepin.subscriptions.prometheus.api.ApiProviderFactory;
+import org.candlepin.subscriptions.prometheus.api.ApiProviderImpl;
+import org.candlepin.subscriptions.prometheus.api.StubApiProvider;
+
+import org.junit.jupiter.api.Test;
+
+public class ApiProviderFactoryTest {
+
+    @Test
+    void canUseStubClient() throws Exception {
+        HttpClientProperties props = new HttpClientProperties();
+        props.setUseStub(true);
+
+        ApiProviderFactory factory = new ApiProviderFactory(props);
+        ApiProvider api = factory.getObject();
+        assertTrue(api instanceof StubApiProvider);
+    }
+
+    @Test
+    public void checkPropertyInitialization() throws Exception {
+        HttpClientProperties props = new HttpClientProperties();
+        assertFalse(props.isUseStub());
+
+        ApiProviderFactory factory = new ApiProviderFactory(props);
+        ApiProvider api = factory.getObject();
+        assertTrue(api instanceof ApiProviderImpl);
+    }
+
+}
