bcc1a8cef226e0970fa842d8f52b9e44f5a63619
Kevin Howell <khowell@redhat.com>
2021-01-13 15:08:10 -0500

    Add Marketplace API client
    The overall workflow is as follows:
    
    1. A Marketplace offering is defined.
    2. The charge ID(s) from the defined offering are provided for Subscription Watch configuration
       (comes from https://sandbox.marketplace.redhat.com/en-us/commerce/edition-charge-editor).
       Because this charge ID is consistent, we should eventually create a config value for it.
       (this field is out of the scope of this change).
    3. A user subscribes via the Marketplace UI.
    4. A notification is sent to RH backend systems.
    5. (Details TBD) Subscription Watch retrieves one or more subscription IDs to be used for integration with the Marketplace API.
    6. Subscription Watch emits usage events to the Marketplace API.
    
    The Marketplace UI can be used to verify charges: https://sandbox.marketplace.redhat.com/en-us/account/pending-charges
    
    Needs:
      - `MARKETPLACE_URL` - e.g. `https://sandbox.marketplace.redhat.com`
      - `MARKETPLACE_API_KEY` - from https://sandbox.marketplace.redhat.com/en-us/account/service-ids, reach out to me for a secret for testing or create your own
      - `subscriptionId` - from the subscription notification (email or webhook), reach out to me to, I will provide for testing
      - `eventId` - generated, I came up locally via `uuidgen | sed 's/\-//g' | dd if=/dev/stdin of=/dev/stdout bs=24 count=1 2>/dev/null`
    
    Additionally, `MARKETPLACE_TOKEN_REFRESH_PERIOD` controls the tolerance for the age of the temporary token used to make API calls (it defaults to 1 minute).
    
    NOTE: according to the Marketplace team, `eventId` should actually be a GUID, and there is work planned to fix it. Currently it is a 24-char ID...
    
    Run as:
    ```
    DEV_MODE=true MARKETPLACE_API_KEY=$MARKETPLACE_API_KEY MARKETPLACE_URL=https://sandbox.marketplace.redhat.com SPRING_PROFILES_ACTIVE=marketplace
    ```
    
    Open up https://sandbox.marketplace.redhat.com/en-us/account/pending-charges and note the current value for pending charges.
    
    Use http://localhost:8080/actuator/hawtio/jmx/operations?nid=root-org.candlepin.subscriptions.marketplace-MarketplaceJmxBean-marketplaceJmxBean with operation
    `submitUsageEvent`, give payloadJson as follows, replacing all values in the JSON of FIXME as appropriate (refer to notes above about the source of values).
    
    ```json
    {
      "start": 1608218158000,
      "end": 1608218158000,
      "resourceType": "SAAS",
      "subscriptionId": "FIXME",
      "eventId": "FIXME",
      "additionalAttributes": {},
      "measuredUsage": [
        {
          "chargeId": "redhat.com:openshiftdedicated:cpu_hour",
          "value": 154
        }
      ]
    }
    ```
    
    Take the resulting response and note the batch ID. Verify the batch is completed via the `getUsageEventStatus` JMX operation.
    
    Verify the resulting charge difference in https://sandbox.marketplace.redhat.com/en-us/account/pending-charges

---
+++ src/main/java/org/candlepin/subscriptions/marketplace/MarketplaceJmxBean.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.marketplace;
+
+import org.candlepin.subscriptions.ApplicationProperties;
+import org.candlepin.subscriptions.marketplace.api.model.UsageEvent;
+import org.candlepin.subscriptions.marketplace.api.model.UsageRequest;
+import org.candlepin.subscriptions.resource.ResourceUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jmx.JmxException;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Exposes admin functions for Marketplace integration.
+ */
+@Component
+@ManagedResource
+public class MarketplaceJmxBean {
+    private static final Logger log = LoggerFactory.getLogger(MarketplaceJmxBean.class);
+
+    private final ApplicationProperties properties;
+    private final MarketplaceService marketplaceService;
+    private final ObjectMapper mapper;
+
+    MarketplaceJmxBean(ApplicationProperties properties, MarketplaceService marketplaceService,
+        ObjectMapper mapper) {
+
+        this.properties = properties;
+        this.marketplaceService = marketplaceService;
+        this.mapper = mapper;
+    }
+
+    @ManagedOperation(description = "Force a refresh of the access token")
+    public void refreshAccessToken() throws ApiException {
+        Object principal = ResourceUtils.getPrincipal();
+        log.info("Marketplace access token forcibly refreshed by {}", principal);
+        marketplaceService.forceRefreshAccessToken();
+    }
+
+    @ManagedOperation(description = "Submit usage event JSON (dev-mode only)")
+    public String submitUsageEvent(String payloadJson) throws JsonProcessingException, ApiException {
+        if (!properties.isDevMode()) {
+            throw new JmxException("Unsupported outside dev-mode");
+        }
+        UsageEvent usageEvent = mapper.readValue(payloadJson, UsageEvent.class);
+        UsageRequest usageRequest = new UsageRequest().addDataItem(usageEvent);
+        return marketplaceService.submitUsageEvents(usageRequest).toString();
+    }
+
+    @ManagedOperation(description = "Fetch a usage event status")
+    public String getUsageEventStatus(String batchId) throws ApiException {
+        return marketplaceService.getUsageBatchStatus(batchId).toString();
+    }
+}
