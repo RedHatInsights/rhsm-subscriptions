2c8d5962e68a2789722dc35be93e3c9e280d668a
Michael Stead <mstead@redhat.com>
2021-02-12 19:17:34 -0400

    Updated copyright dates

--- src/test/java/org/candlepin/subscriptions/prometheus/api/ApiProviderFactoryTest.java
+++ src/test/java/org/candlepin/subscriptions/prometheus/api/ApiProviderFactoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 - 2020 Red Hat, Inc.
+ * Copyright (c) 2021 Red Hat, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
