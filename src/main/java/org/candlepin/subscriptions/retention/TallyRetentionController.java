802913677ce29ec434ab0714933889d83d4f1a69
Lindsey Burnett <lburnett@redhat.com>
2021-04-20 14:20:28 -0400

    Checkstyle's turn!

--- src/main/java/org/candlepin/subscriptions/retention/TallyRetentionController.java
+++ src/main/java/org/candlepin/subscriptions/retention/TallyRetentionController.java
@@ -32,7 +32,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.stream.Stream;
