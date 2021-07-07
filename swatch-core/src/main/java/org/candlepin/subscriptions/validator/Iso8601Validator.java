e29aaede1da1319a270e09b05721ef1acd1fa768
Lindsey Burnett <lburnett@redhat.com>
2021-07-07 13:06:54 -0400

    WIP

---
+++ swatch-core/src/main/java/org/candlepin/subscriptions/validator/Iso8601Validator.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.validator;
+
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/** A ConstraintValidator to ensure Strings are in ISO 8601 format. */
+public class Iso8601Validator implements ConstraintValidator<Iso8601, String> {
+
+  private Iso8601Format format;
+
+  @Override
+  public void initialize(Iso8601 constraintAnnotation) {
+    this.format = constraintAnnotation.value();
+  }
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    // Note that the Jakarta Bean Validation specification recommends to consider null values as
+    // being
+    // valid. If null is not a valid value for an element, it should be annotated with @NotNull
+    // explicitly
+    if (value == null) {
+      return true;
+    }
+
+    DateTimeFormatter formatter = format.formatter;
+
+    try {
+      formatter.parse(value);
+      return true;
+    } catch (DateTimeParseException e) {
+      HibernateConstraintValidatorContext hibernateContext =
+          context.unwrap(HibernateConstraintValidatorContext.class);
+      hibernateContext.addMessageParameter("example", format.example);
+      return false;
+    }
+  }
+}
