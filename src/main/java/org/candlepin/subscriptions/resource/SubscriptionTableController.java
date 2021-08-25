886a01e96d5d1f1b0951041184d6153e94764b0c
Sneha Gunta <sgunta@redhat.com>
2021-08-25 14:15:31 -0400

    Add logs temporarily to debug 500 from subscription table api

--- src/main/java/org/candlepin/subscriptions/resource/SubscriptionTableController.java
+++ src/main/java/org/candlepin/subscriptions/resource/SubscriptionTableController.java
@@ -20,11 +20,6 @@
  */
 package org.candlepin.subscriptions.resource;
 
-import static org.candlepin.subscriptions.resource.ResourceUtils.*;
-
-import java.time.OffsetDateTime;
-import java.util.*;
-import javax.validation.constraints.Min;
 import lombok.extern.slf4j.Slf4j;
 import org.candlepin.subscriptions.db.SubscriptionCapacityViewRepository;
 import org.candlepin.subscriptions.db.model.ServiceLevel;
@@ -35,6 +30,12 @@ import org.candlepin.subscriptions.utilization.api.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.Min;
+import java.time.OffsetDateTime;
+import java.util.*;
+
+import static org.candlepin.subscriptions.resource.ResourceUtils.*;
 
 @Service
 @Slf4j
@@ -96,6 +97,9 @@ public class SubscriptionTableController {
             reportStart,
             reportEnd);
 
+    for(SubscriptionCapacityView capacityView: capacities){
+      log.info("Got Capacity: {}", capacityView.toString());
+    }
     Map<String, SkuCapacity> inventories = new HashMap<>();
     for (SubscriptionCapacityView subscriptionCapacityView : capacities) {
       String sku = subscriptionCapacityView.getSku();
@@ -194,11 +198,16 @@ public class SubscriptionTableController {
 
   public void addTotalCapacity(
       SubscriptionCapacityView subscriptionCapacityView, SkuCapacity skuCapacity) {
+    log.info("Calculating total capacity using {}", skuCapacity.toString());
 
     var physicalSockets = subscriptionCapacityView.getPhysicalSockets();
     var physicalCores = subscriptionCapacityView.getPhysicalCores();
     var virtualSockets = subscriptionCapacityView.getVirtualSockets();
     var virtualCores = subscriptionCapacityView.getVirtualCores();
+    log.info("Got value physicalSockets: {}", physicalSockets);
+    log.info("Got value physicalCores: {}", physicalCores);
+    log.info("Got value virtualSockets: {}", virtualSockets);
+    log.info("Got value virtualCores: {}", virtualCores);
     if (skuCapacity.getUom() == Uom.SOCKETS) {
       skuCapacity.setPhysicalCapacity(skuCapacity.getPhysicalCapacity() + physicalSockets);
       skuCapacity.setVirtualCapacity(skuCapacity.getVirtualCapacity() + virtualSockets);
