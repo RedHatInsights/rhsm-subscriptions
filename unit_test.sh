38fd7b0fda93df4a9f9386979068be7af3803a73
Kevin Howell <khowell@redhat.com>
2021-11-09 12:03:19 -0500

    Add temporary quay image build/push to pr_check.sh
    https://issues.redhat.com/browse/ENT-4482
    
    Note that I added a stub unit_test.sh, as junit XML is expected by the
    Jenkins job which runs the pr_check.sh script
    
    I modified the existing build_deploy.sh script to use common logic,
    defined in cicd_common.sh to detect services. With this change, adding a
    new component to the codebase is as simple as adding a Dockerfile to a
    subdirectory.
    
    Testing
    -------
    To see the commands that will happen, you can insert:
    
    ```
    oc() {
      echo "oc $@"
    }
    
    docker() {
      echo "docker $@"
    }
    
    podman() {
      echo "podman $@"
    }
    ```
    
    to `pr_check.sh` (before any other commands). This will defang those
    commands, outputting the commands with arguments, instead of actually
    executing them.
    
    Then to mimic a run from CI:
    
    ```
    ghprbPullId=42 RH_REGISTRY_USER=foo RH_REGISTRY_TOKEN=bar QUAY_USER=foo QUAY_TOKEN=bar ./pr_check.sh
    ```

---
+++ unit_test.sh
@@ -0,0 +1,9 @@
+# NOTE: this is just a stub from https://github.com/RedHatInsights/bonfire/blob/64e50e1b5536d1eb8d874ea62caf0a572b5e32f1/cicd/examples/unit_test_example.sh#L22-L29
+# If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
+# If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
+mkdir -p $ARTIFACTS_DIR
+cat << EOF > $ARTIFACTS_DIR/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
