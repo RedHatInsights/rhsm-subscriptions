b6513c262429a7288fe1d099cba6cef6830f6886
Lindsey Burnett <lburnett@redhat.com>
2022-01-25 18:25:27 -0500

    Run spotlessApply

--- src/main/java/org/candlepin/subscriptions/resteasy/ResteasyConfiguration.java
+++ src/main/java/org/candlepin/subscriptions/resteasy/ResteasyConfiguration.java
@@ -26,7 +26,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
