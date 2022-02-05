b75f67b57bda4820146351074127a398e9bf47eb
Lindsey Burnett <lburnett@redhat.com>
2022-02-04 19:01:05 -0500

    Generate api Resource classes from openapi spec

--- demo/src/main/java/com/redhat/swatch/resource/FruitResource.java
+++ demo/src/main/java/com/redhat/swatch/resource/FruitResource.java
@@ -1,33 +1,45 @@
 package com.redhat.swatch.resource;
 
+import com.redhat.swatch.openapi.Fruit;
+import com.redhat.swatch.openapi.FruitsApi;
+import java.util.List;
 import javax.inject.Inject;
-import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import com.redhat.swatch.service.FruitService;
 
+
 @Path("/fruits")
-public class FruitResource {
+public class FruitResource implements FruitsApi {
 
   @Inject
   FruitService fruitService;
 
-  @GET
-  @Produces(MediaType.TEXT_PLAIN)
-  @Path("/{fruitName}")
-  public String getAllFruits(@PathParam("fruitName") String fruitName) {
-    return fruitService.getFruit(fruitName);
+  @Override
+  public void createFruit(Fruit fruit) {
+
+    fruitService.createFruitSaladRequest();
+
   }
 
-  @POST
-  @Produces(MediaType.APPLICATION_JSON)
-  @Transactional
-  public String insertFruits() {
-    fruitService.createFruitSaladRequest();
-    return "done";
+  @Override
+  public void deleteFruit(String fruitId) {
+
+  }
+
+  @Override
+  public Fruit getFruit(String fruitId) {
+    com.redhat.swatch.entity.Fruit fruit = fruitService.getFruit(fruitId);
+
+    return new Fruit().name(fruit.name).color(fruit.color);
+  }
+
+  @Override
+  public List<Fruit> getFruits() {
+    return null;
+  }
+
+  @Override
+  public void updateFruit(String fruitId, Fruit fruit) {
+
   }
 }
