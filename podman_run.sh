97dafe8bd0beaeabbac2e80115c35a4c3197b124
Kevin Howell <khowell@redhat.com>
2022-02-25 11:03:18 -0500

    Apply fixes to make testcontainers work in Jenkins
    Needed to passthrough DOCKER_HOST env var, and updated the podman_run.sh
    script so that it starts a temporary socket/daemon, configures
    testcontainers to use it, and then cleans up afterwards.
    
    Couple of interesting bits in the podman command:
    
    1. Disable labeling to avoid selinux issues.
    2. Use host networking so that testcontainers can talk to the containers
       it starts.

--- podman_run.sh
+++ podman_run.sh
@@ -1,1 +1,25 @@
-podman run --rm --user=root -e GRADLE_USER_HOME=/workspace/.gradle -w /workspace -v $(pwd):/workspace:Z registry.access.redhat.com/ubi8/openjdk-11 "$@"
+SOCKET=$(mktemp)
+export CONTAINER_HOST=unix://$SOCKET
+export DOCKER_HOST=$CONTAINER_HOST
+echo Running podman service at $CONTAINER_HOST
+podman system service -t 360 $CONTAINER_HOST&
+PODMAN_PID=$!
+echo Running command '`'$@'`' via podman
+# needs host network and selinux labeling disabled to support testcontainers
+podman run \
+  --net=host \
+  --security-opt label=disable \
+  --rm \
+  --user=root \
+  -e DOCKER_HOST=$CONTAINER_HOST \
+  -e GRADLE_USER_HOME=/workspace/.gradle \
+  -w /workspace \
+  -v $SOCKET:$SOCKET:Z \
+  -v $(pwd):/workspace:Z \
+  registry.access.redhat.com/ubi8/openjdk-11 \
+  "$@"
+RETURN_CODE=$?
+echo Stopping podman service at $CONTAINER_HOST
+kill $PODMAN_PID
+rm -f $SOCKET
+exit $RETURN_CODE
