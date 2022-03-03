c9398da0b95fc81cb02cdf1eef3d793fd1732d13
Kevin Howell <khowell@redhat.com>
2022-03-02 20:15:12 -0500

    Incorporate swatch-producer-aws as subproject
    I did a few things also to ensure we get versions managed properly.
    
    Namely, I introduced a couple of "conventions" gradle plugins, which
    apply common config to projects.
    
    I moved our dependency management to primarily dependencies.gradle,
    which was necessary to give dependabot a consistent place to look.
    
    The spring dependency-managent gradle plugin is also still used, but is
    now limited to certain projects (see projects using the
    `swatch.spring-boot-dependencies-conventions` plugin.
    
    I also cleaned up the `dependencyJson` task a bit, it had been broken.

--- swatch-producer-aws/src/main/java/com/redhat/swatch/processors/MakeMyLifeEasierProducer.java
+++ swatch-producer-aws/src/main/java/com/redhat/swatch/processors/MakeMyLifeEasierProducer.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
 package com.redhat.swatch.processors;
 
 import com.redhat.swatch.openapi.model.TallySummary;
@@ -6,7 +26,6 @@ import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 // TODO not sure if we want to keep this or not yet.  If we want to be able to submit
 // to the endpoint in an async manner and queue the request, this will do that.
@@ -27,7 +46,5 @@ public class MakeMyLifeEasierProducer {
     tallySummarySubmissionFarm.send(tallySummary);
 
     log.info("Queued up a TallySummary object to the tally topic");
-
   }
-
 }
