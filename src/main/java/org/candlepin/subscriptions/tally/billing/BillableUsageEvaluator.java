91cfdb7fbe8633b33b83653166f857b2a996cfd8
Lindsey Burnett <lburnett@redhat.com>
2022-05-25 14:50:22 -0400

    [WIP] Populate tracking table records

--- src/main/java/org/candlepin/subscriptions/tally/billing/BillableUsageEvaluator.java
+++ src/main/java/org/candlepin/subscriptions/tally/billing/BillableUsageEvaluator.java
@@ -1,8 +1,14 @@
 package org.candlepin.subscriptions.tally.billing;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.candlepin.subscriptions.db.BillableUsageRemittanceRepository;
+import org.candlepin.subscriptions.db.model.BillableUsageRemittanceEntity;
 import org.candlepin.subscriptions.json.BillableUsage;
+import org.candlepin.subscriptions.json.TallyMeasurement;
+import org.candlepin.subscriptions.json.TallySnapshot;
 import org.candlepin.subscriptions.json.TallySummary;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +25,45 @@ public class BillableUsageEvaluator {
 
   public BillableUsage evaluateMe(TallySummary tallySummary) {
 
-    log.info("Evaluate Me Please {}", tallySummary);
+    var trackingTableRecords = saveThisPuppyToTheTrackingTable(tallySummary);
+
+    log.info("Tracking Table Records: {}", trackingTableRecords);
 
     return new BillableUsage()
         .withAccountNumber(tallySummary.getAccountNumber())
         .withBillableTallySnapshots(tallySummary.getTallySnapshots());
+  }
+
+  private List<BillableUsageRemittanceEntity> saveThisPuppyToTheTrackingTable(
+      TallySummary tallySummary) {
+
+    var saved = new ArrayList<BillableUsageRemittanceEntity>();
+
+    var accountNumber = tallySummary.getAccountNumber();
+
+    for (TallySnapshot tallySnapshot : tallySummary.getTallySnapshots()) {
+
+      for (TallyMeasurement tallyMeasurement : tallySnapshot.getTallyMeasurements()) {
+        BillableUsageRemittanceEntity record =
+            BillableUsageRemittanceEntity.builder()
+                .usage(tallySnapshot.getUsage().toString())
+                .accountNumber(accountNumber)
+                .billingProvider(tallySnapshot.getProductId())
+                .billingAccountId(tallySnapshot.getBillingAccountId())
+                .granularity(tallySnapshot.getGranularity().toString())
+                .productId(tallySnapshot.getProductId())
+                .sla(tallySnapshot.getSla().toString())
+                .snapshotDate(tallySnapshot.getSnapshotDate())
+                .metricId(tallyMeasurement.getUom().toString())
+                .month("2022-05") // TODO
+                .remittanceDate(OffsetDateTime.now())
+                .remittedValue(tallyMeasurement.getValue())
+                .build();
+
+        saved.add(billableUsageRemittanceRepository.save(record));
+      }
+    }
+
+    return saved;
   }
 }
--- swatch-core/src/main/java/org/candlepin/subscriptions/db/model/BillableUsageRemittanceEntity.java
+++ swatch-core/src/main/java/org/candlepin/subscriptions/db/model/BillableUsageRemittanceEntity.java
@@ -7,9 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "billable_usage_remittance")
 @IdClass(BillableUsageRemittanceEntityPK.class)
