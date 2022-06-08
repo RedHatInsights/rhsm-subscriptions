8b6a46442a33180c9815f1eef238a9656e240182
Kevin Flaherty <kflahert@redhat.com>
2022-06-08 15:56:15 -0400

    log warning when payg subscription is missing billing provider

--- swatch-core/src/main/java/org/candlepin/subscriptions/db/OfferingRepository.java
+++ swatch-core/src/main/java/org/candlepin/subscriptions/db/OfferingRepository.java
@@ -28,4 +28,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OfferingRepository extends JpaRepository<Offering, String> {
 
   List<Offering> findByProductName(String productName);
+
+  Offering findBySku(String sku);
 }
