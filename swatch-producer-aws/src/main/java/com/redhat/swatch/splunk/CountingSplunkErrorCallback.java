de062503f0b0eb3f06ea59d484d3bede9476faa7
Alex Wood <awood@redhat.com>
2022-06-30 16:02:26 -0400

    Add Splunk HEC error handler that counts errors for swatch-producer-aws

--- swatch-producer-aws/src/main/java/com/redhat/swatch/splunk/CountingSplunkErrorCallback.java
+++ swatch-producer-aws/src/main/java/com/redhat/swatch/splunk/CountingSplunkErrorCallback.java
@@ -18,26 +18,25 @@
  * granted to use or replicate Red Hat trademarks that are incorporated
  * in this software or its documentation.
  */
-package org.candlepin.subscriptions.util;
+package com.redhat.swatch.splunk;
 
 import com.splunk.logging.HttpEventCollectorErrorHandler;
 import com.splunk.logging.HttpEventCollectorErrorHandler.ErrorCallback;
 import com.splunk.logging.HttpEventCollectorEventInfo;
-import com.splunk.logging.util.StandardErrorCallback;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.quarkus.runtime.StartupEvent;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * A custom Splunk HEC error handler so that we can track failures in a Prometheus metric and then
- * alert if that metric exceeds acceptable parameters.
- *
- * <p>Retrying of failed requests to splunk is handled by HttpEventCollectorResendMiddleware, and
- * the middleware is configured in the logback configuration file.
+ * A custom Quarkus-ified Splunk HEC error handler so that we can track failures in a Prometheus
+ * metric and then alert if that metric exceeds acceptable parameters.
  */
-@Component
+@Slf4j
+@ApplicationScoped
 public class CountingSplunkErrorCallback implements ErrorCallback {
 
   private final ErrorCallback delegatedCallback;
@@ -46,15 +45,15 @@ public class CountingSplunkErrorCallback implements ErrorCallback {
   public CountingSplunkErrorCallback(MeterRegistry meterRegistry) {
     // Only one ErrorCallback can be registered. The standard callback is useful in that it
     // prints the formatted error to stderr, so we'll delegate invocations of our ErrorCallback
-    // to the StandardErrorCallback so that it can handle creating a human-readable record of the
+    // to the default callback so that it can handle creating a human-readable record of the
     // issue.
-    this.delegatedCallback = new StandardErrorCallback(false);
+    this.delegatedCallback = new QuarkusHecErrorCallback();
     this.failureCounter = meterRegistry.counter("splunk.hec.message.failure.total");
   }
 
   @Override
   public void error(List<HttpEventCollectorEventInfo> data, Exception ex) {
-    // Splunk HEC batches log messages up. The contents of a failed batch come to us in the
+    // Splunk HEC can batch log messages up. The contents of a failed batch come to us in the
     // HttpEventCollectorEventInfo list. The counter is meant to count the total number of
     // messages that failed to send (not the total number of failed transmissions) so we
     // increment by the size of the list.
@@ -62,8 +61,7 @@ public class CountingSplunkErrorCallback implements ErrorCallback {
     delegatedCallback.error(data, ex);
   }
 
-  @PostConstruct
-  public void registerWithSplunk() {
+  void onStart(@Observes StartupEvent ev) {
     HttpEventCollectorErrorHandler.onError(this);
   }
 }
