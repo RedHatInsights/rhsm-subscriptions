e195fe8d741c9f784514c20a4da32cc232dd89ab
Lindsey Burnett <lburnett@redhat.com>
2022-07-27 15:08:13 -0400

    gut the pr check

--- pr_check.sh
+++ pr_check.sh
@@ -1,61 +1,33 @@
 #!/bin/bash
-# NOTE: if you need to debug this file, use DRY_RUN=true to echo docker/podman/oc commands without running them
 
-# before we run common consoledot builds, prepare the binary artifacts for quarkus style builds
-./podman_run.sh ./gradlew assemble
+set -x
 
-source cicd_common.sh
+LABELS_DIR="$WORKSPACE/github_labels"
+ARTIFACTS_DIR="artifacts"
 
-export APP_NAME="rhsm"  # name of app-sre "application" folder this component lives in
+mkdir -p $LABELS_DIR
+mkdir -p $ARTIFACTS_DIR
 
-export IQE_PLUGINS="rhsm-subscriptions"  # name of the IQE plugin for this APP
-export IQE_MARKER_EXPRESSION="ephemeral"  # This is the value passed to pytest -m
-# export IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
-export IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to complete or fail
-# NOTE: workaround for frontend deployment not being ready yet below
-export COMPONENTS="rhsm swatch-producer-aws"
+echo $WORKSPACE
 
-# Install bonfire repo/initialize
-CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
-
-IMAGES=""
-
-export COMPONENT_NAME="rhsm"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-# prebuild artifacts for quarkus builds
-for service in $SERVICES; do
-  export IMAGE="quay.io/cloudservices/$service"  # the image location on quay
-  export DOCKERFILE="$(get_dockerfile $service)"
-
-  # Build the image and push to quay
-  APP_ROOT=$(get_approot $service)
-  source $CICD_ROOT/build.sh
-
-  IMAGES=" ${IMAGES} -i ${IMAGE}=${IMAGE_TAG} "
-done
-
-APP_ROOT=$PWD
-
-EXTRA_DEPLOY_ARGS=${IMAGES}
-COMPONENTS_RESOURCES_ARG=--no-remove-resources=${COMPONENT_NAME}
-OPTIONAL_DEPS_METHOD=none
-
-# Deploy to an ephemeral namespace for testing
-source $CICD_ROOT/deploy_ephemeral_env.sh
-
-# Run smoke tests with ClowdJobInvocation
- source $CICD_ROOT/cji_smoke_test.sh
-# During the PR checks the Ibutsu URL and test run IDs are published as a comment by InsightsDroid account (on GitHub) or iqe-bot (on GitLab).
-# This is achieved by adding this line to `pr_check.sh` of the repo:
- source $CICD_ROOT/post_test_results.sh
+curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/search/issues\?q\=sha:$ghprbActualCommit | jq '.items[].labels[].name' > $LABELS_DIR/github_labels.txt
 
 
-# Need to make a dummy results file to make tests pass
-# Inspired by https://github.com/RedHatInsights/insights-rbac/blo/243b57a20ea2c1da87fe4292a2df9b19e1157efd/pr_check.sh
-# which is listed in the bonfire docs as an example pr_check file
-    mkdir -p artifacts
-    cat << EOF > artifacts/junit-dummy.xml
-    <testsuite tests="1">
-        <testcase classname="dummy" name="dummytest"/>
-    </testsuite>
+function check_for_labels() {
+    if [ -f $LABELS_DIR/github_labels.txt ]; then
+        egrep "$1" $LABELS_DIR/github_labels.txt &>/dev/null
+    fi
+}
+
+if check_for_labels "bananas-test"; then
+  echo "matching bananas"
+fi
+
+
+cp $LABELS_DIR/github_labels.txt $ARTIFACTS_DIR/github_labels.txt
+
+cat << EOF > artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
 EOF
