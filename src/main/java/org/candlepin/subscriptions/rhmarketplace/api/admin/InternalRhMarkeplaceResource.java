fcccb9144cccdf90fefe9a6f276ae29b51dff3de
Kevin Howell <khowell@redhat.com>
2023-05-26 10:51:25 -0400

    Add internal API for swatch-producer-red-hat-marketplace
    Jira issue: [SWATCH-1189](https://issues.redhat.com/browse/SWATCH-1189)
    
    Description
    ===========
    
    The internal API simply references the RHM API response definition.
    
    Testing
    =======
    
    Setup
    -----
    
    Start the service (use the URL and key from the marketplace sandbox):
    
    ```shell
    DEV_MODE=true \
      RH_MARKETPLACE_URL=$FIXME \
      RH_MARKETPLACE_API_KEY=$FIXME \
      ./gradlew :bootRun
    ```
    
    Steps
    -----
    
    1. Query status of an existing batch:
    
    ```shell
    http :8000/api/rhsm-subscriptions/v1/internal/rhm/status/e36bfad2-dafe-57ff-8834-02b3abc1ef93 \
      x-rh-swatch-psk:placeholder
    ```
    
    2. Query a non-existent batch
    
    ```shell
    http :8000/api/rhsm-subscriptions/v1/internal/rhm/status/foobar \
      x-rh-swatch-psk:placeholder
    ```
    
    Verification
    ------------
    
    Observe a successful response for the first request, and a properly formatted JSON error (straight
    from the RHM API) for the second.
    
    Co-authored-by: Barnaby Court <bcourt@redhat.com>

---
+++ src/main/java/org/candlepin/subscriptions/rhmarketplace/api/admin/InternalRhMarkeplaceResource.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.rhmarketplace.api.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.candlepin.subscriptions.rhmarketplace.ApiException;
+import org.candlepin.subscriptions.rhmarketplace.RhMarketplaceService;
+import org.candlepin.subscriptions.rhmarketplace.admin.api.InternalApi;
+import org.candlepin.subscriptions.rhmarketplace.admin.api.model.StatusResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class InternalRhMarkeplaceResource implements InternalApi {
+  private final RhMarketplaceService rhMarketplaceService;
+
+  private final StatusResponseMapper statusResponseMapper;
+
+  InternalRhMarkeplaceResource(
+      RhMarketplaceService rhMarketplaceService, StatusResponseMapper statusMapper) {
+
+    this.rhMarketplaceService = rhMarketplaceService;
+    this.statusResponseMapper = statusMapper;
+  }
+
+  @Override
+  public StatusResponse getUsageEventStatus(String batchId) {
+    // NOTE: no need to validate batchId as the RHM API will do that for us
+    StatusResponse response;
+    try {
+      response =
+          statusResponseMapper.clientToApi(rhMarketplaceService.getUsageBatchStatus(batchId));
+    } catch (ApiException e) {
+      // pass-through of the underlying error response, see RhmExceptionMapper
+      throw new RhmException(e);
+    }
+
+    return response;
+  }
+}
