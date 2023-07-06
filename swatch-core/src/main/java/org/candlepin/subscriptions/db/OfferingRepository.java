b71e8b118a68ec5d0795728655e0d37e096de2e0
Alex Wood <awood@redhat.com>
2023-07-06 10:55:35 -0400

    Cache offering SKUs to reduce DB hits
    This patch adds a short-lived cache of SKUs from the offering table to
    speed up the numerous checks for SKU existence that we make during
    subscription syncing.

--- swatch-core/src/main/java/org/candlepin/subscriptions/db/OfferingRepository.java
+++ swatch-core/src/main/java/org/candlepin/subscriptions/db/OfferingRepository.java
@@ -25,12 +25,69 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.candlepin.subscriptions.db.model.Offering;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 /** Repository interface for the Offering entity */
 public interface OfferingRepository extends JpaRepository<Offering, String> {
+  @Override
+  @Cacheable(cacheNames = {"offeringExists"})
+  boolean existsById(String s);
+
+  @Override
+  @CacheEvict(
+      cacheNames = {"offeringExists"},
+      allEntries = true)
+  void deleteAll();
+
+  @Override
+  @CacheEvict(
+      cacheNames = {"offeringExists"},
+      allEntries = true)
+  void deleteAllInBatch();
+
+  @Override
+  @CacheEvict(
+      cacheNames = {"offeringExists"},
+      allEntries = true)
+  // We would want the cache key to be something like "offerings.forEach(x -> x.getSku())" but
+  // there isn't a good way to do that.  Instead, we'll just invalidate the entire cache.  The
+  // deleteAll() method actually just calls delete() in a loop but the CacheEvict annotation on
+  // our overriden declaration of delete doesn't get invoked.
+  void deleteAll(Iterable<? extends Offering> offerings);
+
+  @Override
+  @CacheEvict(
+      cacheNames = {"offeringExists"},
+      allEntries = true)
+  void deleteAllById(Iterable<? extends String> skus);
+
+  @Override
+  @CacheEvict(
+      cacheNames = {"offeringExists"},
+      allEntries = true)
+  // We would want the cache key to be something like "offerings.forEach(x -> x.getSku())" but
+  // there isn't a good way to do that.  Instead, we'll just invalidate the entire cache.
+  void deleteAllInBatch(Iterable<Offering> offerings);
+
+  @Override
+  @CacheEvict(
+      cacheNames = {"offeringExists"},
+      allEntries = true)
+  void deleteAllByIdInBatch(Iterable<String> skus);
+
+  @Override
+  @CacheEvict(
+      cacheNames = {"offeringExists"},
+      key = "#offering.sku")
+  void delete(Offering offering);
+
+  @Override
+  @CacheEvict(cacheNames = {"offeringExists"})
+  void deleteById(String s);
 
   List<Offering> findByProductName(String productName);
 
