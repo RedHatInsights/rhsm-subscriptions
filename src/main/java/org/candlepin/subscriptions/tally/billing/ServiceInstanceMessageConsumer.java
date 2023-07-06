85e1170a49f2ae6994394c2a8b083cbe147d5735
karshah <karshah@redhat.com>
2023-07-06 15:20:19 -0400

    SWATCH-598 Add new batch Kafka worker/listener for event messages This will be added to swatch tally

---
+++ src/main/java/org/candlepin/subscriptions/tally/billing/ServiceInstanceMessageConsumer.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.tally.billing;
+
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.candlepin.subscriptions.task.TaskQueueProperties;
+import org.candlepin.subscriptions.util.KafkaConsumerRegistry;
+import org.candlepin.subscriptions.util.SeekableKafkaConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class ServiceInstanceMessageConsumer extends SeekableKafkaConsumer {
+
+  @Autowired
+  public ServiceInstanceMessageConsumer(
+      @Qualifier("serviceInstanceTopicProperties")
+          TaskQueueProperties taskServiceInstanceTopicProperties,
+      KafkaConsumerRegistry kafkaConsumerRegistry) {
+    super(taskServiceInstanceTopicProperties, kafkaConsumerRegistry);
+  }
+
+  @KafkaListener(
+      id = "#{__listener.groupId}",
+      topics = "#{__listener.topic}",
+      containerFactory = "kafkaServiceInstanceListenerContainerFactory")
+  @Transactional
+  public void receive(
+      @Payload
+          List<String>
+              events /*, @Header(name = KafkaHeaders.RECEIVED_MESSAGE_KEY, required = false) String kafkaMessageKey*/) {
+    log.info("Events received w/ event list size={}. Consuming events.", events.size());
+  }
+}
