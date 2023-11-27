cb55cedb4dc0530abc1586e65880a696aec9b89c
Kevin Flaherty <kflahert@redhat.com>
2023-11-27 07:05:01 -0500

    Set cores/sockets to null when value is not present to match instance_measurements values

--- src/test/java/org/candlepin/subscriptions/tally/facts/FactNormalizerTest.java
+++ src/test/java/org/candlepin/subscriptions/tally/facts/FactNormalizerTest.java
@@ -98,8 +98,8 @@ class FactNormalizerTest {
     NormalizedFacts normalized =
         normalizer.normalize(createQpcHost("RHEL", "x86_64", clock.now()), hypervisorData());
     assertThat(normalized.getProducts(), Matchers.hasItem("RHEL for x86"));
-    assertEquals(Integer.valueOf(0), normalized.getCores());
-    assertEquals(Integer.valueOf(0), normalized.getSockets());
+    assertNull(normalized.getCores());
+    assertNull(normalized.getSockets());
   }
 
   @Test
@@ -152,7 +152,7 @@ class FactNormalizerTest {
     assertNotNull(normalized.getProducts());
     assertThat(normalized.getProducts(), Matchers.empty());
     assertEquals(Integer.valueOf(4), normalized.getCores());
-    assertEquals(Integer.valueOf(0), normalized.getSockets());
+    assertNull(normalized.getSockets());
   }
 
   @Test
@@ -162,7 +162,7 @@ class FactNormalizerTest {
     NormalizedFacts normalized = normalizer.normalize(host, hypervisorData());
     assertNotNull(normalized.getProducts());
     assertThat(normalized.getProducts(), Matchers.empty());
-    assertEquals(Integer.valueOf(0), normalized.getCores());
+    assertNull(normalized.getCores());
     assertEquals(Integer.valueOf(8), normalized.getSockets());
   }
 
@@ -171,8 +171,8 @@ class FactNormalizerTest {
     NormalizedFacts normalized =
         normalizer.normalize(createRhsmHost(List.of(69), null, clock.now()), hypervisorData());
     assertThat(normalized.getProducts(), Matchers.hasItem("RHEL for x86"));
-    assertEquals(Integer.valueOf(0), normalized.getCores());
-    assertEquals(Integer.valueOf(0), normalized.getSockets());
+    assertNull(normalized.getCores());
+    assertNull(normalized.getSockets());
   }
 
   @Test
@@ -182,7 +182,7 @@ class FactNormalizerTest {
 
     NormalizedFacts normalized = normalizer.normalize(facts, hypervisorData());
     assertThat(normalized.getProducts(), Matchers.empty());
-    assertEquals(0, normalized.getCores());
+    assertNull(normalized.getCores());
   }
 
   @Test
@@ -220,6 +220,8 @@ class FactNormalizerTest {
   @Test
   void testNullSocketsNormalizeToZero() {
     InventoryHostFacts host = createRhsmHost(Collections.emptyList(), null, clock.now());
+
+    host.setSyspurposeUnits("Sockets");
     NormalizedFacts normalizedHost = normalizer.normalize(host, hypervisorData());
 
     assertEquals(0, normalizedHost.getSockets().intValue());
@@ -524,12 +526,12 @@ class FactNormalizerTest {
 
   static Stream<Arguments> syspurposeUnitsArgs() {
     return Stream.of(
-        arguments("Sockets", 2, 0), arguments("Cores/vCPU", 0, 4), arguments("Foobar", 2, 4));
+        arguments("Sockets", 2, null), arguments("Cores/vCPU", null, 4), arguments("Foobar", 2, 4));
   }
 
   @ParameterizedTest
   @MethodSource("syspurposeUnitsArgs")
-  void testSyspurposeUnits(String unit, int sockets, int cores) {
+  void testSyspurposeUnits(String unit, Integer sockets, Integer cores) {
     InventoryHostFacts facts = createBaseHost("O1");
     facts.setSystemProfileCoresPerSocket(2);
     facts.setSystemProfileSockets(2);
@@ -537,8 +539,8 @@ class FactNormalizerTest {
     facts.setSyspurposeUnits(unit);
 
     NormalizedFacts normalized = normalizer.normalize(facts, hypervisorData());
-    assertEquals(sockets, normalized.getSockets().longValue());
-    assertEquals(cores, normalized.getCores().longValue());
+    assertEquals(sockets, normalized.getSockets());
+    assertEquals(cores, normalized.getCores());
   }
 
   @Test
