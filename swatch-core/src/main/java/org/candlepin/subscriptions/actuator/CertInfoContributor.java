e23dfa24db60ac2d4b8c2ad98894ae3ee1d31aad
Alex Wood <awood@redhat.com>
2023-12-05 14:44:45 -0500

    Move CertInfoContributor to swatch-core
    This move allows the CertInfoContributor to be used by the main
    application as well as swatch-system-conduit.

--- swatch-core/src/main/java/org/candlepin/subscriptions/actuator/CertInfoContributor.java
+++ swatch-core/src/main/java/org/candlepin/subscriptions/actuator/CertInfoContributor.java
@@ -18,20 +18,23 @@
  * granted to use or replicate Red Hat trademarks that are incorporated
  * in this software or its documentation.
  */
-package org.candlepin.subscriptions.conduit.actuator;
+package org.candlepin.subscriptions.actuator;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.Map;
-import org.candlepin.subscriptions.actuator.CertInfoInquisitor;
-import org.candlepin.subscriptions.conduit.rhsm.client.RhsmApiProperties;
-import org.candlepin.subscriptions.http.HttpClientProperties;
+import java.util.Map.Entry;
+import java.util.Objects;
+import org.candlepin.subscriptions.util.TlsProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.autoconfigure.info.ConditionalOnEnabledInfoContributor;
 import org.springframework.boot.actuate.autoconfigure.info.InfoContributorFallback;
 import org.springframework.boot.actuate.info.Info.Builder;
 import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 /** Endpoint to print basic information about the certificates Conduit is using. */
@@ -41,47 +44,52 @@ public class CertInfoContributor implements InfoContributor {
   private static final Logger log = LoggerFactory.getLogger(CertInfoContributor.class);
   public static final String CERT_LOAD_ERR = "Could not load certificates";
 
-  private final RhsmApiProperties rhsmApiProperties;
+  private final ApplicationContext context;
 
-  public CertInfoContributor(RhsmApiProperties rhsmApiProperties) {
-    this.rhsmApiProperties = rhsmApiProperties;
+  public CertInfoContributor(ApplicationContext context) {
+    this.context = context;
   }
 
-  public Map<String, Map<String, String>> keystoreInfo() throws IllegalStateException {
-    HttpClientProperties config = rhsmApiProperties;
-
+  public Map<String, Map<String, String>> storeInfo(Resource store, char[] password)
+      throws IllegalStateException {
     try {
-      return CertInfoInquisitor.loadStoreInfo(config.getKeystore(), config.getKeystorePassword());
+      if (Objects.nonNull(store)) {
+        String filename = store.getFilename();
+        if (Objects.nonNull(filename) && !filename.isBlank()) {
+          if (!store.isReadable()) {
+            return Map.of(store.getFilename() + " not readable", Collections.emptyMap());
+          }
+          return CertInfoInquisitor.loadStoreInfo(store, password);
+        }
+      }
+      return Collections.emptyMap();
     } catch (IOException | GeneralSecurityException e) {
-      log.warn(CERT_LOAD_ERR, e);
-      throw new IllegalStateException(CERT_LOAD_ERR, e);
-    }
-  }
-
-  public Map<String, Map<String, String>> truststoreInfo() throws IllegalStateException {
-    HttpClientProperties config = rhsmApiProperties;
-
-    try {
-      return CertInfoInquisitor.loadStoreInfo(
-          config.getTruststore(), config.getTruststorePassword());
-    } catch (IOException | GeneralSecurityException e) {
-      log.warn(CERT_LOAD_ERR, e);
       throw new IllegalStateException(CERT_LOAD_ERR, e);
     }
   }
 
   @Override
   public void contribute(Builder builder) {
-    try {
-      builder.withDetail("keystore", keystoreInfo());
-    } catch (IllegalStateException e) {
-      builder.withDetail("keystore", e.getMessage() + ":" + e.getCause().getMessage());
-    }
+    var clientPropertiesBeans = context.getBeansOfType(TlsProperties.class);
+    for (Entry<String, TlsProperties> entry : clientPropertiesBeans.entrySet()) {
+      String name = entry.getKey();
+      TlsProperties config = entry.getValue();
+      try {
+        builder.withDetail(
+            name + ".keystore", storeInfo(config.getKeystore(), config.getKeystorePassword()));
+      } catch (IllegalStateException e) {
+        log.info(CERT_LOAD_ERR, e);
+        builder.withDetail(name + ".keystore", e.getMessage() + ":" + e.getCause().getMessage());
+      }
 
-    try {
-      builder.withDetail("truststore", truststoreInfo());
-    } catch (IllegalStateException e) {
-      builder.withDetail("truststore", e.getMessage() + ":" + e.getCause().getMessage());
+      try {
+        builder.withDetail(
+            name + ".truststore",
+            storeInfo(config.getTruststore(), config.getTruststorePassword()));
+      } catch (IllegalStateException e) {
+        log.info(CERT_LOAD_ERR, e);
+        builder.withDetail(name + ".truststore", e.getMessage() + ":" + e.getCause().getMessage());
+      }
     }
   }
 }
