364ebeabfba907d5c5e3a6f8912930d3b9a8672f
Michael Stead <mstead@redhat.com>
2024-01-16 15:04:26 -0400

    Add last_applied_event_record_date to InstancesAPI response
    This was done primarily to aid in QE testing

--- src/test/java/org/candlepin/subscriptions/tally/admin/InternalTallyResourceTest.java
+++ src/test/java/org/candlepin/subscriptions/tally/admin/InternalTallyResourceTest.java
@@ -20,7 +20,6 @@
  */
 package org.candlepin.subscriptions.tally.admin;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
