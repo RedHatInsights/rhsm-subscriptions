1558efdf97b6630c209ab4bcdc0bb678698831ff
Kevin Flaherty <kflahert@redhat.com>
2024-01-30 15:01:35 -0500

    spotless

--- src/test/java/org/candlepin/subscriptions/tally/billing/BillableUsageControllerTest.java
+++ src/test/java/org/candlepin/subscriptions/tally/billing/BillableUsageControllerTest.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
