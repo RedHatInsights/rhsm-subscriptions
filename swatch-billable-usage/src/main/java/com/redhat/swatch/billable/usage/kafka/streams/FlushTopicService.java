2427ea9baf49fba7604f8a4da7f7102d4d374742
Kevin Flaherty <kflahert@redhat.com>
2024-04-09 14:37:58 -0400

    Add cron job

--- swatch-billable-usage/src/main/java/com/redhat/swatch/billable/usage/kafka/streams/FlushTopicService.java
+++ swatch-billable-usage/src/main/java/com/redhat/swatch/billable/usage/kafka/streams/FlushTopicService.java
@@ -22,31 +22,47 @@ package com.redhat.swatch.billable.usage.kafka.streams;
 
 import com.redhat.swatch.billable.usage.model.BillableUsage;
 import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
+import jakarta.enterprise.context.ApplicationScoped;
 import org.candlepin.subscriptions.billable.usage.BillableUsageAggregateKey;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
 
+/**
+ * Kafka Streams suppress functionality uses event timestamps in order to advance the internal
+ * clock. If no messages come through for some time then the aggregates will remain suppressed. This
+ * service publishes a message to every partition of the topic in order to publish the suppressed
+ * aggregates.
+ */
+@ApplicationScoped
 public class FlushTopicService {
+
+  Emitter<BillableUsage> emitter;
 
   @ConfigProperty(name = "KAFKA_BILLABLE_USAGE_PARTITIONS")
   int billableUsagePartitionSize;
 
-  @Channel("billable-usage-aggregation-repartition")
-  Emitter<BillableUsage> emitter;
+  private final String FLUSH_ORG = "flush";
+  private final BillableUsage flushUsage;
+  private final BillableUsageAggregateKey flushKey;
+
+  public FlushTopicService(
+      @Channel("billable-usage-aggregation-repartition") Emitter<BillableUsage> emitter) {
+    this.emitter = emitter;
+    this.flushKey = new BillableUsageAggregateKey();
+    flushKey.setOrgId(FLUSH_ORG);
+    this.flushUsage = new BillableUsage();
+    flushUsage.setOrgId(FLUSH_ORG);
+  }
 
   public void sendFlushToBillableUsageRepartitionTopic() {
-    var key = new BillableUsageAggregateKey();
-    key.setOrgId("flush");
-    var usage = new BillableUsage();
-    usage.setOrgId("flush");
     for (int partition = 0; partition < billableUsagePartitionSize; partition++) {
       emitter.send(
-          Message.of(usage)
+          Message.of(flushUsage)
               .addMetadata(
                   OutgoingKafkaRecordMetadata.<BillableUsageAggregateKey>builder()
-                      .withKey(key)
+                      .withKey(flushKey)
                       .withPartition(partition)
                       .build()));
     }
