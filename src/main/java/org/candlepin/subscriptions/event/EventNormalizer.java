0c6239850667a5406b85c152a501e5c3b6b3dde1
Michael Stead <mstead@redhat.com>
2024-05-09 15:52:04 -0300

    Event amendment refactor
    An event's potential overlaps (conflicts) are determined
    based on an EventKey. This key is based on the event's org,
    instance-id and timestamp, all of which identify the usage
    that an event applies to. All non relevant properties have
    been removed from the key to ensure that usage is only based
    on a specific instance/timestamp, no matter where it was
    reported from.
    
    An incoming Event is first normalized (flattened) into a
    set of Events that have a single tag/measurement combo.
    Each of which are then resolved individually, and amendments
    are made to each to ensure that all usage at the specified
    timestamp is first zeroed out before the new usage is applied.
    
    And event is amended, if the measurement values of two compared
    events are different, OR if the hardwareType, sla, usage,
    billing provider, or billingProviderId, is different.
    
    NOTES:
    * Removed usage of EventKey from the EventController.
    * Removed eventType and eventSource from EventKey since they do
      not relate to identifying the usage that the event contributes to.
    * Consider the product tags of events when determining
      which amendments are required to resolve an incoming
      conflict.
    * Consider incoming messages as conflicts after they
      have been processed so they are considered by other
      incoming events.
    * Use LinkedHashMap when grouping events by key in order
      to maintain the order that they are processed.

---
+++ src/main/java/org/candlepin/subscriptions/event/EventNormalizer.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package org.candlepin.subscriptions.event;
+
+import com.google.common.collect.Sets;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.candlepin.subscriptions.json.Event;
+import org.candlepin.subscriptions.json.Measurement;
+
+/**
+ * Defines the logic for flattening an Event into multiple events based on a (tag,measurement)
+ * tuple. This ensures that any event we ingest only contains a single tag and metric, even if the
+ * source sends multiple.
+ *
+ * <pre>
+ *   Incoming event:
+ *      Event(['tag1', 'tag2'], {cores: 2, instance-hours: 4}
+ *   Normalized events:
+ *      Event(['tag1'], {cores: 2})
+ *      Event(['tag1'], {instance-hours: 4})
+ *      Event(['tag2'], {cores: 2})
+ *      Event(['tag2'], {instance-hours: 4})
+ * </pre>
+ */
+public class EventNormalizer {
+
+  private final ResolvedEventMapper resolvedEventMapper;
+
+  public EventNormalizer(ResolvedEventMapper resolvedEventMapper) {
+    this.resolvedEventMapper = resolvedEventMapper;
+  }
+
+  public List<Event> normalizeEvent(Event event) {
+    Set<String> tags = event.getProductTag();
+    Set<Measurement> measurements = new HashSet<>(event.getMeasurements());
+    return Sets.cartesianProduct(tags, measurements).stream()
+        .map(tuple -> create(event, (String) tuple.get(0), (Measurement) tuple.get(1)))
+        .toList();
+  }
+
+  private Event create(Event from, String tag, Measurement measurement) {
+    Event target = new Event();
+    resolvedEventMapper.copy(target, from);
+    target.setProductTag(Set.of(tag));
+    target.setMeasurements(List.of(measurement));
+    return target;
+  }
+}
