b0c9f0157a5e4b38c1cb07a777e5b6a6c28011cb
Kartik Shah <karshah@redhat.com>
2024-09-25 10:45:03 -0400

    SWATCH-2962 NPE in contracts due to empty sku in contracts UMB
    Signed-off-by: Kartik Shah <karshah@redhat.com>

--- swatch-contracts/src/main/java/com/redhat/swatch/contract/service/ContractService.java
+++ swatch-contracts/src/main/java/com/redhat/swatch/contract/service/ContractService.java
@@ -668,6 +668,12 @@ public class ContractService {
       if (entity.getOrgId() == null) {
         throw new ContractNotAssociatedToOrgException();
       }
+      // check to see if sku exists
+      if (!Objects.nonNull(entity.getOffering())
+          || !Objects.nonNull(entity.getOffering().getSku())) {
+        log.warn("Contract doesn't have a offering or sku {}", entity);
+        throw new ContractValidationFailedException();
+      }
       var violations = validator.validate(entity);
       if (!violations.isEmpty()) {
         throw new ContractValidationFailedException(entity, violations);
