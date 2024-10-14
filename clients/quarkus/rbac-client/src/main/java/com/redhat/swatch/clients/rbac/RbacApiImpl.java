6cb86758f8f3afa23ef0837952d95f90842f1720
Jose <josecarvajalhilario@gmail.com>
2024-10-14 18:00:28 +0200

    SWATCH-3026: Create RbacApi and RbacService into the Quarkus Rbac Client
    At the moment, we have some rbac utilities implemented in swatch-core (see package org.candlepin.subscriptions.rbac).
    
    These changes copy this logic into the Quarkus client rbac module.

---
+++ clients/quarkus/rbac-client/src/main/java/com/redhat/swatch/clients/rbac/RbacApiImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.swatch.clients.rbac;
+
+import com.redhat.swatch.clients.rbac.api.model.Access;
+import com.redhat.swatch.clients.rbac.api.resources.AccessApi;
+import com.redhat.swatch.clients.rbac.api.resources.ApiException;
+import java.util.List;
+import lombok.AllArgsConstructor;
+
+/** A wrapper around the RBAC API. */
+@AllArgsConstructor
+public class RbacApiImpl implements RbacApi {
+
+  private final AccessApi accessApi;
+
+  @Override
+  public List<Access> getCurrentUserAccess(String applicationName) throws RbacApiException {
+    try {
+      return accessApi.getPrincipalAccess(applicationName, null, null, null, null).getData();
+    } catch (ApiException apie) {
+      throw new RbacApiException("Unable to get current user access.", apie);
+    }
+  }
+
+  @Override
+  public List<Access> getCurrentIdentityAccess(String application, String identityCode)
+      throws RbacApiException {
+    try {
+      return accessApi.getPrincipalAccess(application, null, identityCode, null, null).getData();
+    } catch (ApiException apie) {
+      throw new RbacApiException("Unable to get current identity access.", apie);
+    }
+  }
+}
