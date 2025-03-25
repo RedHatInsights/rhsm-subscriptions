47837740fbe7a9ac6f866cc1acfa07f84ee34be7
Lindsey Burnett <lburnett@redhat.com>
2025-03-25 17:27:10 -0400

    WIP for failures

---
+++ swatch-contracts/src/test/java/com/redhat/swatch/contract/ForScienceTest.java
@@ -0,0 +1,13 @@
+package com.redhat.swatch.contract;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class ForScienceTest {
+
+  @Test
+  void itShouldFailForScience() {
+    fail("🔥 This test fails for science!");
+  }
+}
