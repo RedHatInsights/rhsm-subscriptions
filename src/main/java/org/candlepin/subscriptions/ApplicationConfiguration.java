3358ea52a688ed2736d1cfb890dbdc4345ea3612
Lindsey Burnett <lburnett@redhat.com>
2025-04-22 07:54:46 -0400

    Revert "SWATCH-2820: Independent database migrations (#4227)"
    This reverts commit 7c5d560f1d208fa104674065b85efb82ad629a86, reversing
    changes made to abdb6a5aac7fbf86f295961decef324ef181525a.

--- src/main/java/org/candlepin/subscriptions/ApplicationConfiguration.java
+++ src/main/java/org/candlepin/subscriptions/ApplicationConfiguration.java
@@ -44,6 +44,7 @@ import org.candlepin.subscriptions.security.AuthProperties;
 import org.candlepin.subscriptions.security.SecurityConfiguration;
 import org.candlepin.subscriptions.subscription.SubscriptionConfiguration;
 import org.candlepin.subscriptions.tally.TallyWorkerConfiguration;
+import org.candlepin.subscriptions.util.LiquibaseUpdateOnlyConfiguration;
 import org.candlepin.subscriptions.util.UtilConfiguration;
 import org.springframework.boot.actuate.autoconfigure.info.ConditionalOnEnabledInfoContributor;
 import org.springframework.boot.actuate.autoconfigure.info.InfoContributorFallback;
@@ -63,6 +64,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Import({
   ApiConfiguration.class,
   CapacityIngressConfiguration.class,
+  LiquibaseUpdateOnlyConfiguration.class,
   TallyWorkerConfiguration.class,
   RhMarketplaceWorkerConfiguration.class,
   DevModeConfiguration.class,
--- swatch-contracts/src/main/java/com/redhat/swatch/contract/utils/SubscriptionDtoUtil.java
+++ swatch-contracts/src/main/java/com/redhat/swatch/contract/utils/SubscriptionDtoUtil.java
@@ -27,6 +27,7 @@ import com.redhat.swatch.contract.repository.BillingProvider;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import liquibase.util.StringUtil;
 
 /** Utility class to assist in pulling nested data out of the Subscription DTO. */
 public class SubscriptionDtoUtil {
@@ -52,7 +53,7 @@ public class SubscriptionDtoUtil {
         products.stream()
             .filter(x -> x.getParentSubscriptionProductId() == null)
             .distinct()
-            .filter(x -> Objects.nonNull(x.getSku()) && !x.getSku().isEmpty())
+            .filter(x -> !StringUtil.isEmpty(x.getSku()))
             .map(SubscriptionProduct::getSku)
             .toList();
 
