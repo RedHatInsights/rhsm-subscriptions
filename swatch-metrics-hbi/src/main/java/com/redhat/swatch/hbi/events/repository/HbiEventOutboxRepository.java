2e0e87b89686fa5de713e957e4a83cb8974ea2fd
Jose Carvajal <jcarvaja@redhat.com>
2025-08-04 08:21:08 +0200

    SWATCH-3776: Create an outbox record table, entity, and a repository supporting CRUD operations

---
+++ swatch-metrics-hbi/src/main/java/com/redhat/swatch/hbi/events/repository/HbiEventOutboxRepository.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.swatch.hbi.events.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+import java.util.UUID;
+
+@ApplicationScoped
+public class HbiEventOutboxRepository implements PanacheRepositoryBase<HbiEventOutbox, UUID> {
+
+  public List<HbiEventOutbox> findByOrgId(String orgId) {
+    return list("orgId", orgId);
+  }
+
+  public long deleteByOrgId(String orgId) {
+    return delete("orgId", orgId);
+  }
+}
