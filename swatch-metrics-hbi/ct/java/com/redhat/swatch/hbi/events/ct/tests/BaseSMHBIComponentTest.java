2edb54c07c5056a36262980c50204ac5c5d4e38d
Michael Stead <mstead@redhat.com>
2025-10-02 15:16:39 -0300

    Add flushOutbox function to the base test class

--- swatch-metrics-hbi/ct/java/com/redhat/swatch/hbi/events/ct/tests/BaseSMHBIComponentTest.java
+++ swatch-metrics-hbi/ct/java/com/redhat/swatch/hbi/events/ct/tests/BaseSMHBIComponentTest.java
@@ -7,8 +7,14 @@ import com.redhat.swatch.component.tests.api.Quarkus;
 import com.redhat.swatch.component.tests.api.SwatchService;
 import com.redhat.swatch.component.tests.api.Unleash;
 import com.redhat.swatch.component.tests.api.UnleashService;
+import com.redhat.swatch.component.tests.utils.AwaitilitySettings;
+import com.redhat.swatch.component.tests.utils.AwaitilityUtils;
 import com.redhat.swatch.component.tests.utils.Topics;
 import com.redhat.swatch.hbi.events.ct.api.SwatchMetricsHbiRestService;
+import com.redhat.swatch.hbi.model.FlushResponse;
+import java.time.Duration;
+import lombok.Getter;
+import org.candlepin.clock.ApplicationClock;
 import org.junit.jupiter.api.Tag;
 
 @ComponentTest
@@ -28,4 +34,47 @@ public class BaseSMHBIComponentTest {
   @Quarkus(service = "swatch-metrics-hbi")
   static SwatchMetricsHbiRestService swatchMetricsHbi = new SwatchMetricsHbiRestService();
 
+  /**
+   * Flush the outbox and continue until the expected flush count is reached, or
+   * we reach the configured await timeout.
+   *
+   * @param expectedFlushCount the expected number of records to flush.
+   */
+  protected void flushOutbox(int expectedFlushCount) {
+    Counter counter = new Counter();
+    AwaitilitySettings settings = AwaitilitySettings.usingTimeout(Duration.ofSeconds(10))
+        .timeoutMessage("Unable to flush the expected number of outbox records in time: %s",
+            expectedFlushCount);
+
+    AwaitilityUtils.untilIsTrue(() -> {
+      while (counter.getCount() < expectedFlushCount) {
+        FlushResponse response = swatchMetricsHbi.flushOutboxSynchronously();
+        if (response.getStatus() == FlushResponse.StatusEnum.SUCCESS &&
+            response.getCount() > 0) {
+          counter.increment(response.getCount());
+          if (counter.getCount() < expectedFlushCount) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }, settings);
+  }
+
+  @Getter
+  private class Counter {
+    private long count = 0;
+
+    public void increment(long amount) {
+      count += amount;
+    }
+
+    public void increment() {
+      count++;
+    }
+
+    public long getCount() {
+      return count;
+    }
+  }
 }
