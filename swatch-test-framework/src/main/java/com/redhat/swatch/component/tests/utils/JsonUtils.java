b6a8a3ed75d043f639ae75b70bb2052241912630
Michael Stead <mstead@redhat.com>
2025-10-06 10:31:20 -0300

    Verify kafka messages using validators.

--- swatch-test-framework/src/main/java/com/redhat/swatch/component/tests/utils/JsonUtils.java
+++ swatch-test-framework/src/main/java/com/redhat/swatch/component/tests/utils/JsonUtils.java
@@ -20,6 +20,7 @@
  */
 package com.redhat.swatch.component.tests.utils;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -47,6 +48,7 @@ public final class JsonUtils {
     mapper.registerModule(new JavaTimeModule());
     mapper.registerModule(new Jdk8Module());
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     return mapper;
   }
 }
