fc92129434d2f9ffa48719a44459da62a8a7726c
Georgios Mantzounis <gmantzou@redhat.com>
2025-11-06 12:23:42 +0100

    SWATCH-4030: Creation of billable usage component test structure.

---
+++ swatch-billable-usage/ct/java/api/ContractsWiremockService.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package api;
+
+import com.redhat.swatch.component.tests.api.WiremockService;
+import java.util.List;
+import java.util.Map;
+
+public class ContractsWiremockService extends WiremockService {
+
+  /**
+   * Setup the contracts API to return a contract with no coverage (all usage is billable). For
+   * contract-enabled products, we must return a contract (not an empty list), but with 0 coverage.
+   *
+   * @param orgId Organization ID
+   * @param productId Product ID
+   */
+  public void setupNoContractCoverage(String orgId, String productId) {
+    // Set contract dates: valid from 1 month ago to 1 year from now
+    String startDate = java.time.OffsetDateTime.now().minusMonths(1).toString();
+    String endDate = java.time.OffsetDateTime.now().plusYears(1).toString();
+
+    // Contract with empty metrics list (no coverage)
+    var contractData =
+        Map.of(
+            "org_id",
+            orgId,
+            "product_id",
+            productId,
+            "start_date",
+            startDate,
+            "end_date",
+            endDate,
+            "metrics",
+            List.of());
+
+    given()
+        .contentType("application/json")
+        .body(
+            Map.of(
+                "request",
+                Map.of(
+                    "method",
+                    "GET",
+                    "urlPathPattern",
+                    "/mock/contractApi/api/swatch-contracts/internal/contracts.*",
+                    "queryParameters",
+                    Map.of("org_id", Map.of("equalTo", orgId))),
+                "response",
+                Map.of(
+                    "status",
+                    200,
+                    "headers",
+                    Map.of("Content-Type", "application/json"),
+                    "jsonBody",
+                    List.of(contractData)),
+                // the default mapping defined in config/wiremock uses priority 10,
+                // so we need a higher priority here.
+                "priority",
+                9,
+                "metadata",
+                getMetadataTags()))
+        .when()
+        .post("/__admin/mappings")
+        .then()
+        .statusCode(201);
+  }
+
+  /**
+   * Setup the contracts API to return contract coverage.
+   *
+   * @param orgId Organization ID
+   * @param productId Product ID
+   * @param metricId Contract metric ID (e.g., "redhat.com:storage_gibibytes_months")
+   * @param coverageValue The amount of coverage provided by the contract
+   */
+  public void setupContractCoverage(
+      String orgId, String productId, String metricId, double coverageValue) {
+    // Set contract dates: valid from 1 month ago to 1 year from now
+    String startDate = java.time.OffsetDateTime.now().minusMonths(1).toString();
+    String endDate = java.time.OffsetDateTime.now().plusYears(1).toString();
+
+    var contractData =
+        Map.of(
+            "org_id",
+            orgId,
+            "product_id",
+            productId,
+            "start_date",
+            startDate,
+            "end_date",
+            endDate,
+            "metrics",
+            List.of(Map.of("metric_id", metricId, "value", coverageValue)));
+
+    given()
+        .contentType("application/json")
+        .body(
+            Map.of(
+                "request",
+                Map.of(
+                    "method",
+                    "GET",
+                    "urlPathPattern",
+                    "/mock/contractApi/api/swatch-contracts/internal/contracts.*",
+                    "queryParameters",
+                    Map.of(
+                        "org_id", Map.of("equalTo", orgId),
+                        "product_tag", Map.of("equalTo", productId))),
+                "response",
+                Map.of(
+                    "status",
+                    200,
+                    "headers",
+                    Map.of("Content-Type", "application/json"),
+                    "jsonBody",
+                    List.of(contractData)),
+                "priority",
+                9,
+                "metadata",
+                getMetadataTags()))
+        .when()
+        .post("/__admin/mappings")
+        .then()
+        .statusCode(201);
+  }
+
+  /**
+   * Setup the contracts API to return 500 error (service unavailable).
+   *
+   * @param orgId Organization ID
+   */
+  public void setupContractServiceError(String orgId) {
+    given()
+        .contentType("application/json")
+        .body(
+            Map.of(
+                "request",
+                Map.of(
+                    "method",
+                    "GET",
+                    "urlPathPattern",
+                    "/mock/contractApi/api/swatch-contracts/internal/contracts.*",
+                    "queryParameters",
+                    Map.of("org_id", Map.of("equalTo", orgId))),
+                "response",
+                Map.of("status", 500),
+                "priority",
+                9,
+                "metadata",
+                getMetadataTags()))
+        .when()
+        .post("/__admin/mappings")
+        .then()
+        .statusCode(201);
+  }
+}
