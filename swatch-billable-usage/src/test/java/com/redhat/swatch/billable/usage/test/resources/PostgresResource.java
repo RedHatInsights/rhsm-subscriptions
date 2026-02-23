343b5ad532f8eb292c782031cdcd28bb2fb2b43d
Jose Carvajal <jcarvaja@redhat.com>
2026-02-23 12:57:32 +0100

    SWATCH-3880: Ensure duplicated tally messages to not create duplicate remittances
    Add a partial unique index on billable_usage_remittance to prevent
    duplicate active remittances for the same tally. The index enforces
    uniqueness on (org_id, product_id, metric_id, accumulation_period, sla,
    usage, billing_provider, billing_account_id, tally_id) only when status
    is not 'failed' or 'succeeded', preserving the retry mechanism for
    failed remittances.
    
    When a concurrent thread attempts to insert a duplicate active
    remittance, the constraint violation is caught in submitBillableUsage
    and the duplicate is silently skipped.

---
+++ swatch-billable-usage/src/test/java/com/redhat/swatch/billable/usage/test/resources/PostgresResource.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.swatch.billable.usage.test.resources;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import java.util.Map;
+import org.candlepin.testcontainers.SwatchPostgreSQLContainer;
+
+public class PostgresResource implements QuarkusTestResourceLifecycleManager {
+
+  static SwatchPostgreSQLContainer db = new SwatchPostgreSQLContainer("rhsm-subscriptions");
+
+  @Override
+  public Map<String, String> start() {
+    db.start();
+    return Map.of(
+        "quarkus.datasource.jdbc.url",
+        db.getJdbcUrl(),
+        "quarkus.datasource.username",
+        db.getUsername(),
+        "quarkus.datasource.password",
+        db.getPassword());
+  }
+
+  @Override
+  public void stop() {
+    db.stop();
+  }
+}
