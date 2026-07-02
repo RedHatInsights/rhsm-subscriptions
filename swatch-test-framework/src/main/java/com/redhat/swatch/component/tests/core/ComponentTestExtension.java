45b364951990422596ef8b0875ecf5391d04dbd6
Trayvon McKnight <tmcknigh@redhat.com>
2026-07-02 15:24:02 -0400

    mvn spotless

--- swatch-test-framework/src/main/java/com/redhat/swatch/component/tests/core/ComponentTestExtension.java
+++ swatch-test-framework/src/main/java/com/redhat/swatch/component/tests/core/ComponentTestExtension.java
@@ -20,6 +20,8 @@
  */
 package com.redhat.swatch.component.tests.core;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import com.redhat.swatch.component.tests.api.Service;
 import com.redhat.swatch.component.tests.api.ServiceLifecycle;
 import com.redhat.swatch.component.tests.api.extensions.AnnotationBinding;
@@ -28,6 +30,16 @@ import com.redhat.swatch.component.tests.logging.Log;
 import com.redhat.swatch.component.tests.utils.InjectUtils;
 import com.redhat.swatch.component.tests.utils.ReflectionUtils;
 import com.redhat.swatch.component.tests.utils.ServiceLoaderUtils;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
@@ -39,19 +51,6 @@ import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolver;
 import org.junit.jupiter.api.extension.TestInstances;
 import org.junit.jupiter.api.extension.TestWatcher;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class ComponentTestExtension
     implements BeforeAllCallback,
