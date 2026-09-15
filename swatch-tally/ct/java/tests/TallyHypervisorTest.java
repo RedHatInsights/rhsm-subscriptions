bfc9525739bdc913c56126a986f51264da13e194
erthomps <eric.thompson.038@gmail.com>
2026-09-15 15:33:31 -0400

    Fixing build

--- swatch-tally/ct/java/tests/TallyHypervisorTest.java
+++ swatch-tally/ct/java/tests/TallyHypervisorTest.java
@@ -48,6 +48,7 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
